bbc28a58cf5ac8ac7bdc90804e62b4b61bb71966
Harrison Liddiard <liddiard@users.noreply.github.com>
2018-06-06 08:14:03 -0700

    make it  u n i v e r s a l  (not just client-side)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-window.console.console = (output = console.log) => {
-  const randomChoice = arr => arr[Math.floor(Math.random()*arr.length) + 1];
-  const messages = [
-    "You're doing great!"
-    // TODO: add more messages here. PRs welcome.
-  ];
-  output(randomChoice(messages));
-};
+if (typeof console === 'object') {
+  console.console = (output = console.log) => {
+    const randomChoice = arr => arr[Math.floor(Math.random()*arr.length) + 1];
+    const messages = [
+      "You're doing great!"
+      // TODO: add more messages here. PRs welcome.
+    ];
+    output(randomChoice(messages));
+  }; 
+}
