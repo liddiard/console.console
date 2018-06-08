4e69cef1ab22f1d70f2aeb5b29142a1a3b078cdc
Harrison Liddiard <liddiard@users.noreply.github.com>
2018-06-08 11:09:19 -0700

    added a message

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ if (typeof console === 'object') {
   console.console = (output = console.log) => {
     const randomChoice = arr => arr[Math.floor(Math.random()*arr.length) + 1];
     const messages = [
-      "You're doing great!"
+      "You're doing great!",
+      "It could be worse."
       // TODO: add more messages here. PRs welcome.
     ];
     output(randomChoice(messages));
