041ba45696e6909e45b236cc609e47e6f11e2c5b
Harrison Liddiard <liddiard@users.noreply.github.com>
2018-07-08 19:18:10 -0700

    add some more (hopefully) consoling msgs

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ if (typeof console === 'object') {
     const randomChoice = arr => arr[Math.floor(Math.random()*arr.length) + 1];
     const messages = [
       "You're doing great!",
-      "It could be worse."
+      "It could be worse.",
+      "Coding in practice is a lot of trial and error. In fact itʼs mostly that. Itʼs not you.",
+      "Your worth as a person is not determined by your coding ability.",
+      "JavaScript sucks. I mean itʼs gotten better but itʼs still kinda lipstick on a pig. A very well-intentioned pig with well-intentioned makeup artists."
       // TODO: add more messages here. PRs welcome.
     ];
     output(randomChoice(messages));
